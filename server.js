897757379efb3a176cdbc967eed20bcccee16e14
Shreyas Saunshi <67939784+ShreyasSaunshi@users.noreply.github.com>
2023-01-06 08:27:16 -0800

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors())
 
+
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'user_name_here',
